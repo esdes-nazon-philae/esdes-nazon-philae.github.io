04e7d72b1f8edf561eba108a713b0761fdf820ad
esdes-nazon-philae <32100496+esdes-nazon-philae@users.noreply.github.com>
2018-03-18 15:14:35 +0100

    Add files via upload

---
+++ 13.02/scripts/index.js
@@ -0,0 +1,10 @@
+var lineDrawing = anime({
+  targets: 'polygon, path, image',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
